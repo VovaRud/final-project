8412aba14a1543915e2bce6ce9071ab98570e306
VovaRud <vovarudenko472@gmail.com>
2024-12-09 12:41:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-document.getElementById('changeBackground').addEventListener('click', function() {
+function changeBackground() {
     const images = [
-        "https://example.com/image1.jpg",
-        "https://example.com/image2.jpg",
-        "https://example.com/image3.jpg"
+        'https://ukrainaincognita.com/wp-content/uploads/2021/06/kobeliaky6.jpg',
+        'https://ukrainaincognita.com/wp-content/uploads/2021/06/kobeliaky5.jpg',
+        'https://ukrainaincognita.com/wp-content/uploads/2021/06/kobeliaky9a.jpg',
+        'https://ukrainaincognita.com/wp-content/uploads/2021/06/kobyliaky_mist5.jpg',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1tWiMD9Lf2o1Y7Vgu7xJR2pG0eQtOy-a9-g&s',
     ];
-    const randomImage = images[Math.floor(Math.random() * images.length)];
-    document.body.style.backgroundImage = `url(${randomImage})`;
-});
+    const randomIndex = Math.floor(Math.random() * images.length);
+    document.body.style.backgroundImage = `url('${images[randomIndex]}')`;
+}
